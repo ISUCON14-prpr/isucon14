c5dbc3da2a461134125012bcbfdebaee930595fb
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-12-08 13:48:01 +0900

    fix: tokio::span

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -53,16 +53,7 @@ async fn main() -> anyhow::Result<()> {
             TcpListener::bind(&SocketAddr::from(([0, 0, 0, 0], 8080))).await?
         };
 
-    let handle = std::thread::spawn(|| async move {
-        loop  {
-            let _ = internal_get_matching_in_thread(pool.clone()).await;
-            println!("internal_get_matching_in_thread done");
-            // 0.5秒待つ
-            tokio::time::sleep(std::time::Duration::from_millis(500)).await;
-        }
-    });
     axum::serve(tcp_listener, app).await?;
-    handle.join().unwrap().await;
 
     Ok(())
 }
